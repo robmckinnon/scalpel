aa4f3be17ec0681c43ee29e5733251ba47b9a4c5
robmckinnon <rob@theyworkforyou.co.nz>
2010-04-05 14:27:31 +0100

    added basic auth check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,20 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  before_filter :authenticate
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   
   def index
+  end
+
+  private
+  def authenticate
+    auth = YAML.load_file(RAILS_ROOT+'/config/auth.yml')
+    auth.symbolize_keys!
+    authenticate_or_request_with_http_basic do |id, password|
+      id == auth[:user] && password == auth[:password]
+    end
   end
 end
